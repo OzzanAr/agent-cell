6037485a1af27ff4b24116e7cc952b4063385554
OzzanAr <ozan.arif@student.um.si>
2024-12-23 12:10:21 +0100

    Changes

--- cpp_implementation/agent_cell/agent_cell/agent_cell.cpp
+++ cpp_implementation/agent_cell/agent_cell/agent_cell.cpp
@@ -3,30 +3,30 @@
 
 using namespace std;
 
-vector<vector<int>> generateGrid(int row, int collumn) {
-    vector<vector<int>> genedGrid;
+vector<vector<int>> generateGrid(int row, int column) {
+    vector<vector<int>> genedGrid(row, vector<int>(column, 0));
 
     return genedGrid;
 }
 
-void printGrid(vector<vector<int>> grid) {
-    for (int i = 0; i < grid.size(); i++) {
-        for (int j = 0; j < grid[i].size(); j++) {
-            cout << grid[i][j] << " ";
+void printGrid(vector<vector<int>> &grid) {
+    for (auto i : grid) {
+        for (auto j : i) {
+            cout << j << " ";
         }
         cout << endl;
     }
+    cout << endl;
 }
 
 int main()
 {
-    cout << "Running Game of Life...\n";
+    cout << "Running Game of Life...\n\n";
 
     int row = 5;
     int col = 5;
 
     vector<vector<int>> grid = generateGrid(row, col);
 
-    
-    
+    printGrid(grid);
 }
